fb05fc8b5c06ed9df3a77be824269862178ac2cd
Faceless-Dolulu <andrei.anastasiu@outlook.com>
2026-02-25 13:43:17 -0500

    Completed implementation of FetchUser Helper

--- apps/api/src/lib/discord-interactions/fetch-user.ts
+++ apps/api/src/lib/discord-interactions/fetch-user.ts
@@ -1,6 +1,62 @@
 import { APIUser } from "discord-api-types/v10";
+import { Request } from "express";
+import { redisClient } from "../..";
+import consola from "consola";
 
-export default async function FetchUser(accessToken: string): Promise<APIUser> {
+export default async function FetchUser(
+	accessToken: string,
+	req: Request,
+): Promise<APIUser> {
+	let user = null;
+	consola.debug("Fetching user data with access token: ", accessToken);
+	consola.debug(
+		"Session data: ",
+		req.session.userId ? { userId: req.session.userId } : "No session userId",
+	);
+
+	if (req.session.userId) {
+		consola.debug("Session userId found: ", req.session.userId);
+		const key = "user:" + req.session.userId;
+		const value = await redisClient.get(key);
+		if (value) {
+			user = JSON.parse(value) as APIUser;
+			consola.debug("Cache hit for user: ", req.session.userId);
+			return user;
+		} else {
+			user = await fetchUserFromDiscord(accessToken);
+			const key = "user:" + user.id;
+			consola.debug(
+				"Cache miss for user: ",
+				req.session.userId,
+				" - Fetched from Discord API",
+			);
+			// Cache user data in Redis for 15 minutes
+			await redisClient.set(key, JSON.stringify(user), {
+				EX: 60 * 15, // 15 minutes
+			});
+
+			consola.debug("User data cached in Redis for user: ", req.session.userId);
+			return user;
+		}
+	} else {
+		consola.debug(
+			"No session userId found, fetching user data directly from Discord API",
+		);
+		user = await fetchUserFromDiscord(accessToken);
+		console.debug("Fetched user data from Discord API: ", user);
+		const key = "user:" + user.id;
+		consola.debug("Updated cache key to: ", key);
+		// Cache user data in Redis for 15 minutes
+		await redisClient.set(key, JSON.stringify(user), {
+			EX: 60 * 15, // 15 minutes
+		});
+
+		consola.debug("User data cached in Redis for user: ", user.id);
+		return user;
+	}
+}
+
+async function fetchUserFromDiscord(accessToken: string): Promise<APIUser> {
 	const userRes = await fetch(`${process.env.DISCORD_API_ENDPOINT}/users/@me`, {
 		method: "GET",
 		headers: {
@@ -8,10 +64,8 @@ export default async function FetchUser(accessToken: string): Promise<APIUser> {
 			"Content-Type": "application/x-www-form-urlencoded",
 		},
 	});
-
 	if (!userRes.ok) {
 		throw new Error(`Failed to fetch user data from Discord: \n ${userRes}`);
 	}
-
 	return (await userRes.json()) as APIUser;
 }
